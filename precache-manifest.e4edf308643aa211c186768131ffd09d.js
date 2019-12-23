0b94075030a63f888d0b5a9bb9b519c336892837
Barbara Borges Ribeiro <barb.b.ribeiro@gmail.com>
2019-12-23 16:02:23 +0000

    Updates

---
+++ precache-manifest.e4edf308643aa211c186768131ffd09d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f14e3a7063c80315a679fd273608dbf",
+    "url": "/ubiquitous-octo-chainsaw/index.html"
+  },
+  {
+    "revision": "69421d55bb1d111b8c9f",
+    "url": "/ubiquitous-octo-chainsaw/static/css/2.61bb36c9.chunk.css"
+  },
+  {
+    "revision": "69421d55bb1d111b8c9f",
+    "url": "/ubiquitous-octo-chainsaw/static/js/2.c194e04c.chunk.js"
+  },
+  {
+    "revision": "86afaa925e0f432774bf95b19b6933fb",
+    "url": "/ubiquitous-octo-chainsaw/static/js/2.c194e04c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "de65ed0ffe9baf36234d",
+    "url": "/ubiquitous-octo-chainsaw/static/js/main.21ab0f45.chunk.js"
+  },
+  {
+    "revision": "d22c5e4efe61a91cf3fb",
+    "url": "/ubiquitous-octo-chainsaw/static/js/runtime-main.8dc36916.js"
+  }
+]);
