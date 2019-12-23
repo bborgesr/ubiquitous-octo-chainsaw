5d07ef4fc2da9cd4175c422e02670a41513e6c05
Barbara Borges Ribeiro <barb.b.ribeiro@gmail.com>
2019-12-23 10:52:50 +0000

    Forms 2 - submit and added toastify

--- src/components/ManagedCoursePage.js
+++ src/components/ManagedCoursePage.js
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 import { Prompt } from "react-router-dom";
 import CourseForm from "./CourseForm";
+import * as courseApi from "../api/courseApi";
+import { toast } from "react-toastify";
 
 const ManagedCoursePage = props => {
   const [course, setCourse] = useState({
@@ -18,12 +20,24 @@ const ManagedCoursePage = props => {
     });
   }
 
+  function handleSubmit(event) {
+    event.preventDefault();
+    courseApi.saveCourse(course).then(() => {
+      props.history.push("/courses");
+      toast.success("Course saved.");
+    });
+  }
+
   return (
     <>
       <h2>Manage Course</h2>
       {/* <Prompt when={true} message="Are you sure you want to leave?" /> */}
       {props.match.params.slug}
-      <CourseForm course={course} onChange={handleChange} />
+      <CourseForm
+        course={course}
+        onChange={handleChange}
+        onSubmit={handleSubmit}
+      />
     </>
   );
 };
