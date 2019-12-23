031b42788de281bca201759073fca05715d318c8
Barbara Borges Ribeiro <barb.b.ribeiro@gmail.com>
2019-12-23 17:30:03 +0000

    hoping

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -5,7 +5,10 @@ function HomePage() {
   return (
     <div className="jumbotron">
       <h1>Pluralsight Admin</h1>
-      <p>React, Flux, and React Router for ultra-fast responsive web apps.</p>
+      <p>
+        React, Flux, and React Router for ultra-fast responsive web apps. I
+        hope...
+      </p>
       <Link to="/about" className="btn btn-primary">
         About
       </Link>
